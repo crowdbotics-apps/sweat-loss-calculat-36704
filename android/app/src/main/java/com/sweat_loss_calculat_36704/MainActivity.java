49bdfa815c3362de5153e119c215d91a57fdd343
Crowdbotics <team@crowdbotics.com>
2022-08-10 12:34:27 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sweat_loss_calculat_36704/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sweat_loss_calculat_36704;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "sweat_loss_calculat_36704";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sweat_loss_calculat_36704",
+  url: "https://sweat_loss_calculat_36704.botics.co",
+  api: "https://sweat_loss_calculat_36704.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
